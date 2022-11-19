1d5bb7d38d82330bd45c4a2811e83df8dc22e09d
Gerard <gerhoquei8@gmail.com>
2022-11-19 22:17:39 +0100

    lladre casi fet

---
+++ IA_Park_Project_GR/Assets/BehaviorBricks/Actions/System/Navigation/Seek.cs
@@ -0,0 +1,29 @@
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
+using UnityEngine;
+
+
+
+
+namespace BBUnity.Actions
+{
+    [Action("Navigation/Seek")]
+    [Help("Gets a random position from a given area and moves the game object to that point by using a NavMeshAgent")]
+
+
+    public class Seek : GOAction
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
